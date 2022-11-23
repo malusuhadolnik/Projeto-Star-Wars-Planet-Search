dd52585fb85f5315db57866469a5d1c7702fa50d
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-11-23 09:45:00 -0300

    renderiza a tabela sem filtros, mas teste no cypress falha

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,30 +1,49 @@
-// import React, { useContext, useEffect, useState } from 'react';
-// import StarWarsContext from '../context/StarWarsContext';
-import React from 'react';
+import React, { useContext } from 'react';
+import StarWarsContext from '../context/StarWarsContext';
+// import React from 'react';
 
 function Table() {
-  // const { data, filters, isLoading } = useContext(StarWarsContext);
+  const { data } = useContext(StarWarsContext);
+  // const [search, setSearch] = useState([]);
+
   return (
     <div>
-      <h2>Renderiza tabela</h2>
-      <Table>
+      <table>
         <tr>
           <td>Name</td>
-          <td>rotation_period</td>
-          <td>orbital_period</td>
+          <td>rotation period</td>
+          <td>orbital period</td>
           <td>diameter</td>
           <td>climate</td>
           <td>gravity</td>
           <td>terrain</td>
-          <td>surface_water</td>
+          <td>surface water</td>
           <td>population</td>
           <td>films</td>
           <td>created</td>
           <td>edited</td>
           <td>url</td>
         </tr>
-      </Table>
-
+        {
+          data.map((planet) => (
+            <tr key={ planet.name }>
+              <td>{ planet.name }</td>
+              <td>{ planet.rotation_period }</td>
+              <td>{ planet.orbital_period }</td>
+              <td>{ planet.diameter }</td>
+              <td>{ planet.climate }</td>
+              <td>{ planet.gravity }</td>
+              <td>{ planet.terrain }</td>
+              <td>{ planet.surface_water }</td>
+              <td>{ planet.population }</td>
+              <td>{ planet.films }</td>
+              <td>{ planet.created }</td>
+              <td>{ planet.edited}</td>
+              <td>{ planet.url }</td>
+            </tr>
+          ))
+        }
+      </table>
     </div>
   );
 }
