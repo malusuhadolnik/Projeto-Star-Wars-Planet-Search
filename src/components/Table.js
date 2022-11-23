ec3d130889590219b207b559cb181a59481ae53c
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-11-23 10:58:07 -0300

    tabela sem filtro agora renderiza corretamente, de acordo com o cypress

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,45 +4,51 @@ import StarWarsContext from '../context/StarWarsContext';
 
 function Table() {
   const { data } = useContext(StarWarsContext);
+  console.log(data);
   // const [search, setSearch] = useState([]);
 
   return (
     <div>
+      <h2>renderiza o componente table</h2>
       <table>
-        <tr>
-          <td>Name</td>
-          <td>rotation period</td>
-          <td>orbital period</td>
-          <td>diameter</td>
-          <td>climate</td>
-          <td>gravity</td>
-          <td>terrain</td>
-          <td>surface water</td>
-          <td>population</td>
-          <td>films</td>
-          <td>created</td>
-          <td>edited</td>
-          <td>url</td>
-        </tr>
-        {
-          data.map((planet) => (
-            <tr key={ planet.name }>
-              <td>{ planet.name }</td>
-              <td>{ planet.rotation_period }</td>
-              <td>{ planet.orbital_period }</td>
-              <td>{ planet.diameter }</td>
-              <td>{ planet.climate }</td>
-              <td>{ planet.gravity }</td>
-              <td>{ planet.terrain }</td>
-              <td>{ planet.surface_water }</td>
-              <td>{ planet.population }</td>
-              <td>{ planet.films }</td>
-              <td>{ planet.created }</td>
-              <td>{ planet.edited}</td>
-              <td>{ planet.url }</td>
-            </tr>
-          ))
-        }
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>rotation period</th>
+            <th>orbital period</th>
+            <th>diameter</th>
+            <th>climate</th>
+            <th>gravity</th>
+            <th>terrain</th>
+            <th>surface water</th>
+            <th>population</th>
+            <th>films</th>
+            <th>created</th>
+            <th>edited</th>
+            <th>url</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((planet) => (
+              <tr key={ planet.name }>
+                <td>{ planet.name }</td>
+                <td>{ planet.rotation_period }</td>
+                <td>{ planet.orbital_period }</td>
+                <td>{ planet.diameter }</td>
+                <td>{ planet.climate }</td>
+                <td>{ planet.gravity }</td>
+                <td>{ planet.terrain }</td>
+                <td>{ planet.surface_water }</td>
+                <td>{ planet.population }</td>
+                <td>{ planet.films }</td>
+                <td>{ planet.created }</td>
+                <td>{ planet.edited}</td>
+                <td>{ planet.url }</td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     </div>
   );
--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -3,14 +3,12 @@ import PropTypes from 'prop-types';
 import StarWarsContext from './StarWarsContext';
 
 export default function StarWarsProvider({ children }) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
   const [filters, setFilters] = useState({});
 
   useEffect(() => {
     async function fetchData() {
       try {
-        setIsLoading(true);
         const response = await fetch('https://swapi.dev/api/planets');
         const dataJson = await response.json();
         const onlyResults = dataJson.results;
@@ -19,23 +17,19 @@ export default function StarWarsProvider({ children }) {
           delete planet.residents;
           return planet;
         });
-
         setData(filteredResults);
       } catch (error) {
         throw new Error(error.message);
-      } finally {
-        setIsLoading(false);
       }
     }
     fetchData();
   }, []);
 
   const values = useMemo(() => ({
-    isLoading,
     data,
     filters,
     setFilters,
-  }), [isLoading, data, filters, setFilters]);
+  }), [data, filters, setFilters]);
 
   return (
     <StarWarsContext.Provider value={ values }>
