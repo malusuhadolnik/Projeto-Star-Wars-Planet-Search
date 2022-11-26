7c8a3e72f737d3edb9c29d8ef0de79412098cbc3
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-11-26 11:25:55 -0300

    refatoração da renderização do botão de opções e renderização de filtros selecionados na tela

--- src/components/FilterNumbersForm.js
+++ src/components/FilterNumbersForm.js
@@ -14,41 +14,7 @@ export default function FilterNumbersForm() {
     setNumInputs({ ...numInput, [target.name]: target.value });
   };
 
-  // const filterData = useCallback(() => {
-  //   if (selectedFilters.length > 0 && numInput.comparison === 'igual a') {
-  //     const filtered = data.filter((planet) => {
-  //       const bools = [];
-  //       selectedFilters.forEach((singleFilter) => {
-  //         bools.push(Number(planet[singleFilter.column]) === Number(singleFilter.value));
-  //       });
-  //       return bools.every((element) => element); // (element) => element === true, tiramos  a comparação pq vai ficar redundante
-  //       // resolução inspirada na gravação Mentoria de Revisão: Filtros e HOFs para o StarWars
-  //     });
-  //     setSearch(filtered);
-  //   }
-  //   if (selectedFilters.length > 0 && numInput.comparison === 'menor que') {
-  //     const filtered = data.filter((planet) => {
-  //       const bools = [];
-  //       selectedFilters.forEach((singleFilter) => {
-  //         bools.push(Number(planet[singleFilter.column]) < Number(singleFilter.value));
-  //       });
-  //       return bools.every((element) => element);
-  //     });
-  //     setSearch(filtered);
-  //   }
-  //   if (selectedFilters.length > 0 && numInput.comparison === 'maior que') {
-  //     const filtered = data.filter((planet) => {
-  //       const bools = [];
-  //       selectedFilters.forEach((singleFilter) => {
-  //         bools.push(Number(planet[singleFilter.column]) > Number(singleFilter.value));
-  //       });
-  //       return bools.every((element) => element);
-  //     });
-  //     setSearch(filtered);
-  //   }
-  // }, [data, numInput.comparison, selectedFilters, setSearch]);
-
-  const filterNumericData = useCallback(() => {
+  const filterNumericData = useCallback(() => { // desenvolvida após mentoria com Tiago Quadros
     const filter = data.filter((planet) => {
       const filterResults = selectedFilters.map(({ column, comparison, value }) => {
         switch (comparison) {
@@ -74,60 +40,72 @@ export default function FilterNumbersForm() {
     ]));
   };
 
-  // useEffect(() => {
-  //   filterData();
-  // }, [filterData]);
-
   useEffect(() => {
     setSearch(filterNumericData());
   }, [filterNumericData, setSearch]);
 
+  const columnOptions = ['population',
+    'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+
   return (
-    <form>
-      <label htmlFor="column-filter">
-        Coluna
-        <select
-          data-testid="column-filter"
-          name="column"
-          value={ numInput.column }
-          id="gender"
-          onChange={ handleChange }
+    <div>
+      <form>
+        <label htmlFor="column-filter">
+          Coluna
+          <select
+            data-testid="column-filter"
+            name="column"
+            value={ numInput.column }
+            id="gender"
+            onChange={ handleChange }
+          >
+            {
+              columnOptions.map((option) => (
+                <option value={ option } key={ option }>{ option }</option>
+              ))
+            }
+          </select>
+        </label>
+        <label htmlFor="comparison">
+          Operador:
+          <select
+            data-testid="comparison-filter"
+            id="comparison"
+            value={ numInput.quantity }
+            name="comparison"
+            onChange={ handleChange }
+          >
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+          <input
+            data-testid="value-filter"
+            type="number"
+            value={ numInput.value }
+            name="value"
+            onChange={ handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ handleClick }
         >
-          <option value="population">population</option>
-          <option value="orbital_period">orbital_period</option>
-          <option value="diameter">diameter</option>
-          <option value="rotation_period">rotation_period</option>
-          <option value="surface_water">surface_water</option>
-        </select>
-      </label>
-      <label htmlFor="comparison">
-        Operador:
-        <select
-          data-testid="comparison-filter"
-          id="comparison"
-          value={ numInput.quantity }
-          name="comparison"
-          onChange={ handleChange }
-        >
-          <option value="maior que">maior que</option>
-          <option value="menor que">menor que</option>
-          <option value="igual a">igual a</option>
-        </select>
-        <input
-          data-testid="value-filter"
-          type="number"
-          value={ numInput.value }
-          name="value"
-          onChange={ handleChange }
-        />
-      </label>
-      <button
-        type="button"
-        data-testid="button-filter"
-        onClick={ handleClick }
-      >
-        Filtrar
-      </button>
-    </form>
+          Filtrar
+        </button>
+      </form>
+      <div>
+        <h2>Filtros selecionados</h2>
+        {
+          selectedFilters.map((filter, index) => (
+            <p key={ index }>
+              { `${filter.column} ${filter.comparison} ${filter.value}` }
+            </p>
+          ))
+        }
+      </div>
+    </div>
+
   );
 }
